7a95ee33deab1341b957af36400cdb0dce20cdb8
Mathias Svensson <freaken@freaken.dk>
2010-12-09 22:10:15 +0100

    Lavede run_edge_detector om til en funktion der tager imod fil-argumenter

--- Opgave2/framework/run_edge_detector.m
+++ Opgave2/framework/run_edge_detector.m
@@ -1,27 +1,6 @@
-clear all;
-close all;
-
-RGB = imread('lena_part.jpg');
+function [] = run_edge_detector(imgin, imgout)
+RGB = imread(imgin);
 I = double( rgb2gray(RGB) );
 J = edge_detector(I);
-
-figure(1);
-clf;
-imagesc( I );
-hold on;
-axis tight;
-colorbar;
-colormap gray;
-title('Input Image');
-hold off;
-
-figure(2);
-clf;
-imagesc( J );
-hold on;
-axis tight;
-colorbar;
-colormap gray;
-title('Edge Image');
-hold off;
-print('-f2', '-dpng' ,'my_edges');
+J = J ./ max(max(J));
+imwrite(J, imgout, 'png');
